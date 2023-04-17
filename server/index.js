8f91fcf3f651cc252c7ed53f8f49b7a2f06f2068
Jose <105132443+josegonzalezmonroy@users.noreply.github.com>
2023-04-17 20:58:51 -0300

    criando variáveis de ambiente (.env)

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
+require("dotenv").config();
 
-const mongoose = require("mongoose");
-const db = "imoveisdb";
-const cloud = `mongodb+srv://usuario_teste:5QzyH5D7BAeZnwDM@cluster0.t6zgnjs.mongodb.net/${db}`;
+const user = process.env.USER;
+const senha = process.env.SENHA;
+const db = process.env.DATABASE;
+const cloud = `mongodb+srv://${user}:${senha}@cluster0.pbawe09.mongodb.net/${db}`;
 
-const routes = require('./routes/routes')
-app.use(routes)
+const routes = require("./routes/routes");
+app.use(routes);
 
 const port = 3002;
 app.listen(port, () => console.log(`Servidor no http://localhost:${port}/`));
