45cbc2610ea1de89d978467178966df6ec2e8de0
Jose <105132443+josegonzalezmonroy@users.noreply.github.com>
2023-04-18 22:08:19 -0300

    criando a funcao editar

--- server/controllers/imoveisController.js
+++ server/controllers/imoveisController.js
@@ -32,11 +32,36 @@ async function deleteImovel(req, res) {
     excluir
       ? res.json(`id: ${id} removido`)
       : res.status(404).json(`id: ${id} nao encontrado`);
-  } catch (erro) {
-    console.error(erro);
+  } catch (err) {
+    console.error(err);
     res.status(400).json("Formato de id nao valido");
   }
 }
 
-module.exports = { novoImovel, listaDeImoveis, deleteImovel };
- 
+async function editarImovel(req, res) {
+  const { id } = req.params;
+  const { tipo, titulo, descricao, preco, quartos, pet, vagas, localizacao } =
+    req.body;
+try {
+  const imovel = await Imoveis.findById(id);
+  imovel
+    ? (await imovel.updateOne({
+        tipo,
+        titulo,
+        descricao,
+        preco,
+        quartos,
+        pet,
+        vagas,
+        localizacao,
+      }),
+      res.status(202).json("Modificado com sucesso"))
+    : res.status(404).json("Erro");
+
+} catch (err) {
+     console.error(err)
+        return res.status(400).json("Erro")
+}
+}
+
+module.exports = { novoImovel, listaDeImoveis, deleteImovel, editarImovel };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,6 +4,7 @@ const imoveis = require('../controllers/imoveisController')
 
 router.get('/', imoveis.listaDeImoveis)
 router.post('/create', imoveis.novoImovel)
-router.delete('/imoveis/:id', imoveis.deleteImovel) 
+router.delete('/imoveis/:id', imoveis.deleteImovel)
+router.patch('/edit/:id', imoveis.editarImovel) 
 
 module.exports = router
